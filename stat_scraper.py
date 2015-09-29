3a1c87d57320195ff913533512198fb8e11572a3
JH <hannajm9@gmail.com>
2015-09-28 21:27:24 -0700

    Add link building function

--- stat_scraper.py
+++ stat_scraper.py
@@ -8,6 +8,7 @@ sys.setdefaultencoding("utf-8")
  
 f = open('stat_value.txt', 'r')
 stat_value = {}
+stat_link = {}
 
 def stat_value_dict(filename):
     for line in filename:
@@ -17,12 +18,28 @@ def stat_value_dict(filename):
         else:
             print False
     
-    return stat_value  
+    return stat_value
+
+def build_link():
+    link = 'stats.ncaa.org/rankings/national_ranking?academic_year=2016.0&amp;division=11.0&amp;ranking_period=14.0&amp;sport_code=MFB&amp;stat_seq='
+    for key, value in stat_value.iteritems():
+        stat_link[key] = link + value
+
+    return stat_link
+                 
             
 stat_value_dict(f)
+build_link()
 
-# print stat_value['Total Defense']
-# print stat_value['Fewest Penalty Yards Per Game']
+'''test for functions
+
+for key, value in stat_link.iteritems():
+    print key, value
+
+print stat_value['Total Defense']
+print stat_value['Fewest Penalty Yards Per Game']
+
+'''
 
 third_down_durl = 'http://stats.ncaa.org/rankings/national_ranking?academic_year=2016.0&amp;division=11.0&amp;ranking_period=14.0&amp;sport_code=MFB&amp;stat_seq=22.0'
 third_down_dpage = urlopen(third_down_durl)
