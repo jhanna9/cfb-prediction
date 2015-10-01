3153b090ca4a62a30693d21c9f43a8b2aab8db72
JH <hannajm9@gmail.com>
2015-10-01 09:34:23 -0700

    Simplify link building function. Store in a separate file

---
+++ link_builder.py
@@ -0,0 +1,26 @@
+# a function to build links to specific statistics of ncaa football
+import re
+
+def build_link(filename, path):
+    '''Combines link and stat code to create stat specific link
+
+    Keyword arguments:
+    filename -- file used to store stat codes
+    
+    returns: a dictionary
+
+
+    '''
+    # dictionaries used to create individual stat link
+    stat_link = {}
+    
+    # matches stat code, creates, and stores link in dictionary with corresponding stat
+    for line in filename:
+        match = re.search(r'([\d]+).0">([\w\s]+)', line, re.IGNORECASE)
+        if match:
+            stat_link[match.group(2)] = path + match.group(1)
+        else:
+            print False
+
+    return stat_link
+
