549333625727a5dbb838af8fded8ed20c9d2441a
JH <hannajm9@gmail.com>
2015-10-01 09:35:06 -0700

    Simplify function. Now calling link builder from another file

--- stat_scraper.py
+++ stat_scraper.py
@@ -3,8 +3,9 @@
 import csv
 import re
 import sys
-from urllib import urlopen
 from bs4 import BeautifulSoup
+from link_builder import build_link
+from urllib import urlopen
 
 # defaults encoding to utf-8
 reload(sys)
@@ -12,46 +13,17 @@ sys.setdefaultencoding("utf-8")
 
 # file that contains numeric code for each stat 
 f = open('stat_value.txt', 'r')
+link = 'http://stats.ncaa.org/rankings/national_ranking?academic_year=2016.0&amp;division=11.0&amp;ranking_period=14.0&amp;sport_code=MFB&amp;stat_seq='
 
-# dictionaries used to create individual stat link
-stat_value = {}
+# dictionary to store individual stats links
 stat_link = {}
 
+stat_link = build_link(f, link)
 
-def stat_value_dict(filename):
-    '''Pulls stat code from file and stores it in stat_value dictionary
+for k, v in stat_link.iteritems():
+    print 'stat: ', k + '\n'
+    print 'link: ', v + '\n\n'
 
-    Keyword arguments:
-    filename -- file used to store stat codes
-    
-    returns: a dictionary
-
-
-    '''
-    for line in filename:
-        match = re.search(r'([\d]+).0">([\w\s]+)', line, re.IGNORECASE)
-        if match:
-            stat_value[match.group(2)] = match.group(1)
-        else:
-            print False
-    
-    return stat_value
-
-def build_link():
-    '''Combines link and stat code to create stat specific link
-
-    Keyword arguments:
-    None
-    
-    returns: a dictionary
-
-
-    '''
-    link = 'http://stats.ncaa.org/rankings/national_ranking?academic_year=2016.0&amp;division=11.0&amp;ranking_period=14.0&amp;sport_code=MFB&amp;stat_seq='
-    for key, value in stat_value.iteritems():
-        stat_link[key] = link + value
-
-    return stat_link
 
 def bs_objects(stat):
     '''Creates a Beautiful Soup object from stat parameter and writes it to a file
@@ -73,10 +45,10 @@ def bs_objects(stat):
     file.close
 
 # function calls
-stat_value_dict(f)
-build_link()
-# bs_objects('Total Defense') # writes Total Defense stat page to file 
-bs_objects('Scoring Defense') # writes Scoring Defense stat page to file
+# stat_value_dict(f)
+# build_link()
+# bs_objects('Total Defense') # writes Beautiful Souped Total Defense stat page to file 
+# bs_objects('Scoring Defense') # writes Beautiful Souped Scoring Defense stat page to file
 
 
 '''function tests
@@ -94,6 +66,41 @@ third_down_dpage = urlopen(third_down_durl)
 soup = BeautifulSoup(third_down_dpage)
 
 # print soup.find_all('table')
+
+def stat_value_dict(filename):
+    Pulls stat code from file and stores it in stat_value dictionary
+
+    Keyword arguments:
+    filename -- file used to store stat codes
+    
+    returns: a dictionary
+
+
+    
+    for line in filename:
+        match = re.search(r'([\d]+).0">([\w\s]+)', line, re.IGNORECASE)
+        if match:
+            stat_value[match.group(2)] = match.group(1)
+        else:
+            print False
+    
+    return stat_value
+
+def build_link():
+Combines link and stat code to create stat specific link
+
+    Keyword arguments:
+    None
+    
+    returns: a dictionary
+
+
+    
+    
+    for key, value in stat_value.iteritems():
+        stat_link[key] = link + value
+
+    return stat_link
 
 '''
 
