75ad16a8307a009ecb1ba14be8fe6e5c2075ef2f
JH <hannajm9@gmail.com>
2015-10-07 15:43:22 -0700

    Clean up comments

--- stat_scraper.py
+++ stat_scraper.py
@@ -57,7 +57,9 @@ def bs_objects(stat):
 
     for tag in secondtable.find_all(re.compile('td')):        
         data_lst.append(tag.string.strip())
-            
+     
+    print data_lst[1]
+    print data_lst[11]       
     
     file = open(stat + '_stats.txt', 'w')
         
@@ -84,61 +86,4 @@ def bs_objects(stat):
          
 # function calls
 for key in key_stats:
-    bs_objects(key) # loops key stats list and writes stats to file 
-# bs_objects('Scoring Defense') # writes Beautiful Souped Scoring Defense stat page to file
-
-
-'''function tests/old functions
-
-for key, value in stat_link.iteritems():
-    print "key: ", key + '\n'
-    print "link: ", value + '\n\n'
-
-print stat_value['Total Defense']
-print stat_value['Fewest Penalty Yards Per Game']
-
-third_down_durl = 'http://stats.ncaa.org/rankings/national_ranking?academic_year=2016.0&amp;division=11.0&amp;ranking_period=14.0&amp;sport_code=MFB&amp;stat_seq=22.0'
-third_down_dpage = urlopen(third_down_durl)
-
-soup = BeautifulSoup(third_down_dpage)
-
-# print soup.find_all('table')
-
-def stat_value_dict(filename):
-    Pulls stat code from file and stores it in stat_value dictionary
-
-    Keyword arguments:
-    filename -- file used to store stat codes
-    
-    returns: a dictionary
-
-
-    
-    for line in filename:
-        match = re.search(r'([\d]+).0">([\w\s]+)', line, re.IGNORECASE)
-        if match:
-            stat_value[match.group(2)] = match.group(1)
-        else:
-            print False
-    
-    return stat_value
-
-def build_link():
-Combines link and stat code to create stat specific link
-
-    Keyword arguments:
-    None
-    
-    returns: a dictionary
-
-
-    
-    
-    for key, value in stat_value.iteritems():
-        stat_link[key] = link + value
-
-    return stat_link
-
-'''
-
-
+    bs_objects(key) # loops key stats list and writes stats to file
