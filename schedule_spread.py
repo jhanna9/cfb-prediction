e4c66e2af426bb2f4522bebd7446c88fa14de468
JH <hannajm9@gmail.com>
2015-10-08 23:06:47 -0700

    First iteration of spread function

--- schedule_spread.py
+++ schedule_spread.py
@@ -12,7 +12,7 @@ sys.setdefaultencoding("utf-8")
 # use to get around firewalls blocking scrapes
 headers = {'User-agent': 'Mozilla/5.0'}
 
-# link to schedule and spread
+# links to schedule and spread
 link_path = 'http://www.vegasinsider.com/college-football/odds/las-vegas/'
 
 def schedule(link):
@@ -21,12 +21,43 @@ def schedule(link):
 
     soup = BeautifulSoup(sched.content)
    
-    for tag in soup('b'): #find_all(re.compile('b')):
-        print tag.string
+    for tag in soup(''):
+        games.append(tag.string)
 
-    return games
+    '''Prints the schedule in format: 'Team 1 vs. Team 2'
+    x = 0
+    y = 1
+    games = games[:-3]
 
-print schedule(link_path)
+    while x < len(games):
+        print games[x] + ' vs. ' + games[y]
+        x += 2
+        y += 2
+    '''
+
+    return games[:-3]
+
+def spread(link):
+    spread = requests.get(link, headers=headers)
+
+    soup = BeautifulSoup(spread.content)
+
+    tabledata = soup.find_all('td')
+
+    x = 0
+    y = 27
+
+    while x < 4:
+        print tabledata[y]
+        x += 1
+        y += 12
+        
+    #for tag in tabledata:
+        #print tag
+
+# function calls
+#print schedule(link_path)
+spread(link_path)
 
 
-# spread = requests.get(link, headers=headers)
+
