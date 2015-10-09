4fe5dc094e520ee408c8c04b2d281eb4a8d28ba8
JH <hannajm9@gmail.com>
2015-10-09 16:07:16 -0700

    Create list of matchups

--- schedule_spread.py
+++ schedule_spread.py
@@ -16,31 +16,25 @@ headers = {'User-agent': 'Mozilla/5.0'}
 link_sched = 'http://www.vegasinsider.com/college-football/odds/las-vegas/'
 link_spread = 'http://www.vegas.com/gaming/sportsline/college-football/'
 
+# global list
+# games = []
+
 def schedule(link):
     games = []
     sched = requests.get(link, headers=headers)
 
     soup = BeautifulSoup(sched.content)
    
-    for tag in soup(''):
+    for tag in soup('b'):
         games.append(tag.string)
-
-    '''Prints the schedule in format: 'Team 1 vs. Team 2'
-    x = 0
-    y = 1
-    games = games[:-3]
-
-    while x < len(games):
-        print games[x] + ' vs. ' + games[y]
-        x += 2
-        y += 2
-    '''
 
     return games[:-3]
 
+
 def spread(link):
     tabledata_lst = []
-    spread_lst = []
+    spread_lst = []  
+    
     spread = requests.get(link, headers=headers)
 
     soup = BeautifulSoup(spread.content)
@@ -56,18 +50,33 @@ def spread(link):
         spread_lst.append(tabledata_lst[x])
         x += 45
 
-    print len(spread_lst)
-    print spread_lst[49]
-    print spread_lst[50]
-    print spread_lst[51]
-    print spread_lst[52]
-
     return spread_lst
 
 
+
+def match_spread(sched, spread):
+    # Prints the schedule in format: 'Team 1 vs. Team 2'
+    matchup_spread = {}
+    matchup = []
+
+    x = 0
+    y = 1
+
+    while x < len(sched):
+        matchup.append(sched[x] + ' vs. ' + sched[y])
+        x += 2
+        y += 2  
+    
+    for m in matchup:
+        matchup_spread = {m: s for s in spread}
+
+    return matchup_spread
+
+
 # function calls
+print match_spread(schedule(link_sched), spread(link_spread))
 #print schedule(link_sched)
-spread(link_spread)
+#print spread(link_spread)
 
 
 
