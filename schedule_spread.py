e9b25961aeeec3058f3e0f7fcd549d16abee5104
JH <hannajm9@gmail.com>
2015-10-09 21:52:59 -0700

    Add docu string and comments. Fixes Issue #3

--- schedule_spread.py
+++ schedule_spread.py
@@ -1,6 +1,5 @@
-# a script to scrape the schedule and spread of ncaa football teams from vegas insider/mirage sportsbook
+# a script to scrape the schedule and spread of ncaa football teams from covers
 # imports
-import re
 import requests
 import sys
 from bs4 import BeautifulSoup
@@ -17,10 +16,23 @@ data = 'http://www.covers.com/odds/football/college-football-odds.aspx'
 
 
 def schedule(link):
+    '''Takes a URL input and scrapes for team names
+
+    Keyword arguments:
+    link -- url to game schedule
+
+    returns: a list
+
+
+    '''
+    # list to return
     games = []
+    
+    # pull data from site and create BS object
     sched = requests.get(link, headers=headers)
     soup = BeautifulSoup(sched.content)
 
+    # iterate through BS object looking for strong tag
     for tag in soup('strong'):
         games.append(tag.string)
 
@@ -28,10 +40,23 @@ def schedule(link):
 
 
 def spread(link):
+    '''Takes a URL input and scrapes for current spread
+
+    Keyword arguments:
+    link -- url to game schedule
+
+    returns: a list
+
+
+    '''
+    # list to return
     spread_lst = []
+    
+    # pull data from site and create BS object
     spread = requests.get(link, headers=headers)
     soup = BeautifulSoup(spread.content)
 
+    # iterate through BS object looking for div and class='covers_bottom'
     for tag in soup.find_all('div', class_='covers_bottom'):
         spread_lst.append(tag.string.strip())
 
@@ -39,17 +64,31 @@ def spread(link):
    
     
 def match_spread(sched, spread):
+    '''Takes two objects that return lists
+
+    Keyword arguments:
+    sched -- schedule function called with URL
+    spread -- spread function called with URL
+
+    returns: a dictionary
+
+
+    '''
+    # dictionary to return and a placeholder list
     matchup_spread = {}
     matchup = []
 
+    # variables
     x = 0
     y = 1
 
+    # create a list with game matchups
     while x < len(sched):
         matchup.append(sched[x] + ' vs. ' + sched[y])
         x += 2
         y += 2  
     
+    # create dictionary of game matchups as the key and spreads as the value
     matchup_spread = dict(zip(matchup, spread))
 
     return matchup_spread
