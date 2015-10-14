558bdfdefdc149f917b665041f07b5a8c8931637
JH <hannajm9@gmail.com>
2015-10-13 23:06:44 -0700

    Create function to make win streak dictionary

--- sos_strk.py
+++ sos_strk.py
@@ -14,7 +14,7 @@ headers = {'User-agent': 'Mozilla/5.0'}
 
 # link to sos and streak
 str_sched = 'https://www.teamrankings.com/college-football/ranking/schedule-strength-by-other'
-streak = 'https://www.teamrankings.com/ncf/standings/'
+wl_strk = 'http://www.ncaa.com/standings/football/fbs'
 
 def sos(link):
     # dictionary to return
@@ -22,8 +22,6 @@ def sos(link):
 
     # list for storage
     sos_lst = []
-    #sos_stor = []
-    #team_stor = []
     team_lst = []
 
     # pull data from site and create BS object
@@ -41,27 +39,49 @@ def sos(link):
     # slice lists to get proper data
     team_lst = team_lst[1::2]
     sos_lst = sos_lst[2::6]
-    
-
-    '''x = 1
-
-    while x < len(team_stor):
-        team_lst.append(team_stor[x])
-        x += 2
-
-    y = 2
-
-    while sos_stor[y] != '40':
-        sos_lst.append(sos_stor[y])
-        y += 6
-
-    '''
 
     # create dictionary of game team as the key and strenght of schedule as the value
     str_sched = dict(zip(team_lst, sos_lst))
   
-
     return str_sched
 
+
+def streak(link):
+    # dictionary to return
+    strk_lst = {}
+
+    # list for storage
+    strk_lst = []
+    team_lst = []
+
+    # pull data from site and create BS object
+    win_streak = requests.get(link, headers=headers)
+    soup = BeautifulSoup(win_streak.content)
+    # table = soup.find_all('table')
+
+    # iterate through BS object looking for a and td tags
+    for tag in soup('a'):
+        team_lst.append(tag.string)   
+
+    for tag in soup('td'):
+        strk_lst.append(tag.string)
+
+    # slice lists to get proper data
+    team_lst = team_lst[117:245]
+    strk_lst = strk_lst[7::8]
+
+    for s in strk_lst:
+        if s == 'STREAK':
+            strk_lst.remove(s)    
+
+    # create dictionary of game team as the key and strenght of schedule as the value
+    strk_lst = dict(zip(team_lst, strk_lst))
+  
+    return strk_lst
+
+# function calls
 for k, v in sos(str_sched).iteritems():
     print k, v
+for k, v in streak(wl_strk).iteritems():
+    print k, v
+
