08023b8bf068404aa2d7599178513c7d1a84a7b0
JH <hannajm9@gmail.com>
2015-11-06 22:49:52 -0800

    Create z score function for comparison

--- compare_picks.py
+++ compare_picks.py
@@ -2,6 +2,7 @@
 # imports
 import numpy
 from schedule_spread import *
+from scipy import stats
 from sos import sos
 from stat_scraper import *
 
@@ -41,7 +42,57 @@ def stand_dev(dict):
     
     return stdev
 
+def z_score(dic):
+    score = []
+    team = []
+    
+    for k, v in dic.iteritems():
+        team.append(k)
+        score.append(float(v))
 
+    score = stats.zscore(score)
+
+    score2 = ['%.2f' % elem for elem in score]
+
+    tm_zscore = dict(zip(team, score2))
+
+    return tm_zscore
+    
+
+def stat_std_lst():
+    std_lst = []
+
+    for s in key_stats:
+        std_lst.append(round(stand_dev(stat_dict_build(s)), 2))
+
+    return std_lst 
+
+
+def compare(tm_stat, tm_sos):
+
+    pass
+
+    
+    
+             
+      
+# function calls
+#z_score(sos(str_sched))
+for k, v in z_score(sos(str_sched)).iteritems():
+    print k, v
+    print '\n'
+
+print sos_avg(sos(str_sched))
+print stand_dev(sos(str_sched))
+
+# print std_range(sos_avg(sos(str_sched)), stand_dev(sos(str_sched))) 
+
+# print stat_std_lst()
+
+#compare(combo_stats(), sos(str_sched))
+
+# possibly unnecessary functions
+'''
 def std_range(avg, std_sos):
     
     sdev_pos = []
@@ -64,20 +115,10 @@ def std_range(avg, std_sos):
 
     return sdev_all_2
 
+'''
 
-def stat_std_lst():
-    std_lst = []
 
-    for s in key_stats:
-        std_lst.append(round(stand_dev(stat_dict_build(s)), 2))
 
-    return std_lst
-             
-      
-# function calls
-print std_range(sos_avg(sos(str_sched)), stand_dev(sos(str_sched))) 
-
-print stat_std_lst()
 
 
     
