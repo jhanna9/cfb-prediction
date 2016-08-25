f7f6d9dea0a5eb0254a3aa8aeed35c9c9536415c
Jim Hanna <hannajm9@gmail.com>
2016-08-25 14:21:55 -0700

    Added loop to get specific stats

--- bets_2016-17.py
+++ bets_2016-17.py
@@ -4,15 +4,16 @@ from bs4 import BeautifulSoup
 # use to get around firewalls blocking scrapes
 headers = {'User-agent': 'Mozilla/5.0'}
 
-# links to stats sites
+# DATA IN DIFFERENT FILElinks to stats sites
 links = ["http://www.ncaa.com/stats/football/fbs/current/team/23",
         "http://www.ncaa.com/stats/football/fbs/current/team/23/p2",
         "http://www.ncaa.com/stats/football/fbs/current/team/23/p3"]
 
 # list to hold all teams and stats
 team_stats = []
+yprush = []
 
-# loop through the links
+# FUNCTION loop through the links
 for l in links:
     site = requests.get(l, headers=headers) # get site info using requests
     # create BS object, sort through necessary info
@@ -22,11 +23,23 @@ for l in links:
     # loop through BS object and add teams and stats to list
     for i in ts:
         team_stats.append(i.get_text())
- 
-print(team_stats)
+
+
+# FUNCTION loop to get yards per rushing attempt, get median and stand dev
+x = 5
+for s in team_stats:
+    if x > len(team_stats):
+        break
+    else:
+        yprush.append(team_stats[x])
+        x += 8
+
+print(yprush)
+print(len(yprush))
+        
 
 '''
-need to sort through the list
-extract teams and stats to combine them to a dictionary
+need to sort through the list to
+extract teams / stats to combine them to a dictionary
 need to handle '-' for ties in team rankings
 '''
