e1e61944376a616c44f2bb67681076d27ee5ae4d
Jim Hanna <hannajm9@gmail.com>
2016-08-29 14:56:30 -0700

    eliminated unecessary if/else statement in stat dict build function

--- links.py
+++ links.py
@@ -2,15 +2,9 @@
 def build_stat_dict(file):
     d = {}
     with open(file) as f:
-        #if file == 'stat_num.txt':
         for line in f:
             (key, val) = line.split()
             d[key] = val
-        '''else:
-            for line in f:
-                (key, val1, val2) = line.split()
-                d[key] = val1, val2
-        '''
             
     return d
 
