a2ef612bf5c0e20fd44c38f94fb5321764f8e486
Jim Hanna <hannajm9@gmail.com>
2016-09-02 19:50:28 -0700

    Continued work on team stat function

--- team_stat.py
+++ team_stat.py
@@ -7,15 +7,31 @@ def team_stat_dict():
 
     '''
 
+    # dictionary to hold teams and stats
+    team_stat = {}
+
     # link to schedule and spread
     data = 'http://www.covers.com/odds/football/college-football-odds.aspx'
 
     teams = schedule(data)
-    print(teams)
 
     # builds then iterates through each stat's BS obj
-    for k in build_stat_dict('stat_position.txt').items():
+    for k, v in build_stat_dict('stat_position.txt').items():
         # creates a BS object per stat
         stats = soupy(k)
+        print(k)
 
-team_stat_dict()    
+        for t in teams:  
+            if t in stats:
+                #print(t + ' is in stats list' + '\n')
+                p = stats.index(t)
+                print(p)
+                #print(stats[int(p) + (int(v) - 2)])
+                team_stat[t] = stats[int(p) + (int(v) - 2)]
+            else:
+                #print(t + ' is not in stats list' + '\n')
+                pass
+        
+    return team_stat
+            
+print(team_stat_dict())
