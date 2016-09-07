9eb83aa6c9f3620c1136aa6cb9469532d78919bb
Jim Hanna <hannajm9@gmail.com>
2016-09-07 10:30:15 -0700

    Worked on function to build a dictionary of teams with multiple identifiers

--- dict_test.py
+++ dict_test.py
@@ -1,17 +1,11 @@
-def build_stat_dict(file):
+def multi_team_dict(file):
     d = {}
     with open(file) as f:
-        if file == 'stat_num.txt':
-            for line in f:
-                (key, val) = line.split()
-                d[key] = val
-        else:
-            for line in f:
-                (key, val1, val2) = line.split()
-                d[key] = val1, val2
+        for line in f:
+            (key, val1, val2) = line.split()
+            d[key] = val1, val2
             
     return d
 
-print(build_stat_dict('stat_num.txt'))
+print(build_stat_dict('team_variation.txt'))
 
-print(build_stat_dict('stat_position.txt'))
