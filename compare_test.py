0ebbed7b46cc3421dda555fc7aef3ce560d65ebb
Jim Hanna <hannajm9@gmail.com>
2016-09-22 20:29:44 -0700

    Finished compare test

--- compare_test.py
+++ compare_test.py
@@ -1,13 +1,16 @@
 d = {'a': ('a', 'a1', 'a2'), 'b': ('b', 'b1', 'b2'), 'c': ('c', 'c1', 'c2')}
 
-listo = ['a2', 'c1', 'd', 'b', 'e']
+listo = ['a2', 'c1', 'd', 'b']
 
 no_list = []
 good_list = []
 x = 0
 
 for l in listo:
+    ind = listo.index(l)
     x = 0
+    if listo[ind - 1] in no_list:
+        break
     for k, v in d.items():     
         if l in v:
             good_list.append(k)
@@ -17,6 +20,11 @@ for l in listo:
             no_list.append(l)          
         else:
             x += 1
+
+#for l in listo:
+#ind = listo.index(l)
+#print(listo[(ind - 1)])
+
 
 print(no_list)
 print(good_list)
