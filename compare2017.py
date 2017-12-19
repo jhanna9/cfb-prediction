8c7e1edf5c4f0c1061dc87c9858881f74cbe2a66
jhanna9 <hannajm9@gmail.com>
2017-12-19 05:59:48 -0800

    z_score_column opens stat position and returns a list

--- compare2017.py
+++ compare2017.py
@@ -1,66 +1,78 @@
-def csv_stat_calc():
+from data_pull import stat_num_reader
+
+def z_score_column(stat_position_file):
 	'''
 
 
 	'''
-	stats = list(stat_num_reader('stat_num.txt'))
+	stats = list(stat_num_reader(stat_position_file))
 
-	for name in stats:
-		file_name = name[0] + '.csv'
-		stat_list = []
-
-		with open(os.path.join(my_path, file_name), 'r') as f:
-			file_reader = csv.reader(f)
-			next(file_reader)
-
-			for row in file_reader:
-				stat_list.append(float(row[-1]))
-
-			stat_mean = mean(stat_list)
-			stat_sdev = stdev(stat_list)
-
-			# print(name[0], stat_mean, stat_sdev)
-
-		yield name[0], stat_mean, stat_sdev
+	return stats
 
 
-def passes_int_clean(csv_file):
-	'''Opens a csv file, finds the correct statistic by column, and saves a new csv file
-
-	returns a string
-
-	'''
-	with open(os.path.join(my_path, csv_file), 'r') as f, open(os.path.join(my_path, 'Passes_Intercepted_new.csv'), 'w', newline='') as w:
-		file_reader = csv.reader(f)
-		file_writer = csv.writer(w)
-
-		for row in file_reader:
-			file_writer.writerow(row[:-2])
+# def csv_stat_calc():
+# 	'''
 
 
-	return 'done'
+# 	'''
+# 	stats = list(stat_num_reader('stat_num.txt'))
+
+# 	for name in stats:
+# 		file_name = name[0] + '.csv'
+# 		stat_list = []
+
+# 		with open(os.path.join(my_path, file_name), 'r') as f:
+# 			file_reader = csv.reader(f)
+# 			next(file_reader)
+
+# 			for row in file_reader:
+# 				stat_list.append(float(row[-1]))
+
+# 			stat_mean = mean(stat_list)
+# 			stat_sdev = stdev(stat_list)
+
+# 			# print(name[0], stat_mean, stat_sdev)
+
+# 		yield name[0], stat_mean, stat_sdev
 
 
-class Stats:
-	def __init__(self, stat_name, stat_mean, stat_stdev):
-		self.name = stat_name
-		self.mean = stat_mean
-		self.stdev = stat_stdev
+# def passes_int_clean(csv_file):
+# 	'''Opens a csv file, finds the correct statistic by column, and saves a new csv file
 
-	def store_calc():
-		stat_calc = list(csv_stat_calc())
+# 	returns a string
 
-		# stats_class = []
+# 	'''
+# 	with open(os.path.join(my_path, csv_file), 'r') as f, open(os.path.join(my_path, 'Passes_Intercepted_new.csv'), 'w', newline='') as w:
+# 		file_reader = csv.reader(f)
+# 		file_writer = csv.writer(w)
 
-		for s in stat_calc:
-			name = str(s[0])
-			name = Stats(s[0], s[1], s[2])
-			# print('Stat: {} \nMean: {} \nStandard Deviation: {} \n'.format(name.name, name.mean, name.stdev))
-			# stats_class.append(s)
-
-			yield name.name, name.mean, name.stdev
+# 		for row in file_reader:
+# 			file_writer.writerow(row[:-2])
 
 
+# 	return 'done'
+
+
+# class Stats:
+# 	def __init__(self, stat_name, stat_mean, stat_stdev):
+# 		self.name = stat_name
+# 		self.mean = stat_mean
+# 		self.stdev = stat_stdev
+
+# 	def store_calc():
+# 		stat_calc = list(csv_stat_calc())
+
+# 		# stats_class = []
+
+# 		for s in stat_calc:
+# 			name = str(s[0])
+# 			name = Stats(s[0], s[1], s[2])
+# 			# print('Stat: {} \nMean: {} \nStandard Deviation: {} \n'.format(name.name, name.mean, name.stdev))
+# 			# stats_class.append(s)
+
+# 			yield name.name, name.mean, name.stdev
+
+print(z_score_column('stat_position.txt'))
 # statistics = list(Stats.store_calc())
 # print(statistics.name)
 # for s in statistics:
