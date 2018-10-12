7339abf02aa8fd2a379c8db09ffacdadeea29814
jhanna9 <hannajm9@gmail.com>
2018-10-11 17:36:26 -0700

    worked on splitting schedule_spread csv into individual teams by row

--- compare.py
+++ compare.py
@@ -14,21 +14,16 @@ def schedule_spread_teams():
         for row in reader:
             row = str(row)
             schedule_spread = row
-            teams = row.split()
+            teams = row.split(' at ')
 
-            # need to figure out teams with top 25 rankings
-            if len(teams) == 6:
-                team_1 = teams[0] + ' ' + teams[1]
-                team_2 = teams[3] + ' ' + teams[4]
-            else:
-                team_1 = teams[0]
-                team_2 = teams[2]
+            team_1 = teams[0]
+            team_2 = teams[1].split(',')
 
             print(team_1)
-            print(team_2)
+            print(team_2[0])
             print('\n')
 
-        # return schedule_spread, team_1, team_2
+        return schedule_spread, team_1, team_2[0]
 
 
 if __name__ == "__main__":
