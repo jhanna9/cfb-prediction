9cdc001ba46b7de961d5c9dd5c5aa2767ffd40eb
jhanna9 <hannajm9@gmail.com>
2018-10-22 21:36:47 -0700

    added schedule spread print to standout function

--- compare.py
+++ compare.py
@@ -5,37 +5,21 @@
 import csv
 
 
-def schedule_spread_teams():
+def schedule_spread():
     '''
+
 
     '''
     with open('stat_csv/schedule_spread.csv') as f:
         matchup_spread = []
-        teams_list = []
         reader = csv.reader(f)
         for row in reader:
             matchup_spread.append(row)
-            row = str(row)
-            teams = row.split(' at ')
 
-            team_1 = teams[0]
-            team_2 = teams[1].split(',')
-
-            if team_1[2] == '(':
-                teams_list.append(team_1[6:])
-            else:
-                teams_list.append(team_1[2:])
-
-            teams_list.append(team_2[0][:-1])
-
-        for item in matchup_spread:
-            print(item[0])
-
-        for item in teams_list:
-            print(item)
-
-        return None  # matchup_spread, teams_list
+    return matchup_spread
 
 
 if __name__ == "__main__":
-    print(schedule_spread_teams())
+    for game in schedule_spread():
+        print(game[0], game[1])
+        print('\n')
