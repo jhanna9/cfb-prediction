0f2ca1f6d4b7ddab9e063a0d1cb254bb0bc6bc42
jhanna9 <hannajm9@gmail.com>
2018-11-13 11:08:01 -0800

    split into individual teams by schedule

--- compare.py
+++ compare.py
@@ -14,12 +14,21 @@ def schedule_spread():
         matchup_spread = []
         reader = csv.reader(f)
         for row in reader:
-            matchup_spread.append(row)
+            # matchup_spread.append(row)
+            if ' vs ' in row[0]:
+                for col in row[0].split(' vs '):
+                    print(col)
+            else:
+                for col in row[0].split(' at '):
+                    print(col)
+
+            print('\n')
 
     return matchup_spread
 
 
 if __name__ == "__main__":
-    for game in schedule_spread():
-        print(game[0], game[1])
-        print('\n')
+    # for game in schedule_spread():
+    #     print(game[0], game[1])
+    #     print('\n')
+    schedule_spread()
