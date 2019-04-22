d6ec8a921ae635cbed416f96830b51af38422a8d
jhanna9 <hannajm9@gmail.com>
2019-04-22 14:52:56 -0700

    worked with pandas to extract proper data

--- compare.py
+++ compare.py
@@ -3,6 +3,10 @@
 
 '''
 import csv
+import pandas as pd
+
+
+stat_file = 'C:\\Users\\J\\Documents\\python\\cfb-prediction\\stat_csv\\Rushing_Defense.csv'
 
 
 def schedule_spread():
@@ -44,8 +48,21 @@ def remove_rank(ranked_team):
     return no_rank
 
 
-if __name__ == "__main__":
+def get_useful_stats(csv_file):
+    useful_stat = pd.read_csv(csv_file)
+    print(useful_stat[['YPG']])  # extracts a single column
+
+    # for row in useful_stat:
+    #     print(row)
+
+
+def main():
     # for game in schedule_spread():
     #     print(game[0], game[1])
     #     print('\n')
-    schedule_spread()
+    # schedule_spread()
+    get_useful_stats(stat_file)
+
+
+if __name__ == "__main__":
+    main()
